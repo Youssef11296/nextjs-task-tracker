b2a5c78b5b317facdfe47d4f95b851272b0e6291
Youssef Shaaban <youssef@gmail.com>
2021-12-31 10:33:19 +0200

    DB setup & task model

---
+++ models/Task.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+
+const taskSchema = new mongoose.Schema (
+  {
+    title: {
+      type: String,
+      required: true,
+      minlength: [3, 'The task title must contain 3 letters at least'],
+      minlength: [3, 'The task title can not contain more than 20 letters'],
+    },
+    describtion: {
+      type: String,
+      required: true,
+      minlength: [10, 'The task descrintion must contain 10 letters at least'],
+      minlength: [
+        200,
+        'The task descrintion can not contain more than 200 letters',
+      ],
+    },
+    reminder: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.models.Task || mongoose.model ('Task', taskSchema);
